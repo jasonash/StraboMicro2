aa9c6cb145c04a490e1705606d65c42274ed91ab
Jason <jason@jasonash.com>
2025-11-24 14:10:01 -0600

    [Cleanup] Remove debug logging from AddMicrographsToGroupDialog

--- src/components/dialogs/AddMicrographsToGroupDialog.tsx
+++ src/components/dialogs/AddMicrographsToGroupDialog.tsx
@@ -56,10 +56,7 @@ export function AddMicrographsToGroupDialog({
 
   // Get all micrographs from the index
   const allMicrographs = useMemo(() => {
-    const micrographs = Array.from(micrographIndex.values());
-    console.log('[AddMicrographsToGroupDialog] micrographIndex size:', micrographIndex.size);
-    console.log('[AddMicrographsToGroupDialog] allMicrographs:', micrographs.map(m => m.name));
-    return micrographs;
+    return Array.from(micrographIndex.values());
   }, [micrographIndex]);
 
   // Filter out micrographs already in the selected group
@@ -68,12 +65,8 @@ export function AddMicrographsToGroupDialog({
 
     const group = groups.find(g => g.id === selectedGroupId);
     const existingIds = new Set(group?.micrographs || []);
-    console.log('[AddMicrographsToGroupDialog] group:', group?.name);
-    console.log('[AddMicrographsToGroupDialog] existingIds:', Array.from(existingIds));
 
-    const available = allMicrographs.filter(m => !existingIds.has(m.id));
-    console.log('[AddMicrographsToGroupDialog] available:', available.map(m => m.name));
-    return available;
+    return allMicrographs.filter(m => !existingIds.has(m.id));
   }, [allMicrographs, selectedGroupId, groups]);
 
   // Reset state when dialog opens
